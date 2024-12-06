7742089c724d463dd8234c4da8f1a1e0744f25db
IronBeagle404 <ironbeagle.pro@gmail.com>
2024-12-06 21:46:38 +0100

    completed day04/part2

--- day04/main.go
+++ day04/main.go
@@ -12,31 +12,26 @@ func main() {
 		log.Fatal(err)
 	}
 	inputStr := string(input)
-
 	matrix := StrToRuneMatrix(inputStr)
 
 	fmt.Printf("XMAS found : %v\n", FindXmas(matrix))
 }
 
-// Search every occurence of XMAS/SAMX in the input
+// Search every occurence of a MAS/SAM cross in the input
 func FindXmas(matrix [][]rune) int {
 	var xmasFound int
 	for x, line := range matrix {
 		for y, char := range line {
-			// Horizontal
-			if (y <= len(line)-4) && (string(char)+string(matrix[x][y+1])+string(matrix[x][y+2])+string(matrix[x][y+3]) == "XMAS" || string(char)+string(matrix[x][y+1])+string(matrix[x][y+2])+string(matrix[x][y+3]) == "SAMX") {
+			if (x >= 1 && x <= len(matrix)-2) && (y >= 1 && y <= len(line)-2) && (string(matrix[x-1][y-1])+string(char)+string(matrix[x+1][y+1]) == "MAS") && (string(matrix[x+1][y-1])+string(char)+string(matrix[x-1][y+1]) == "MAS") {
 				xmasFound++
 			}
-			// Vertical
-			if (x <= len(matrix)-4) && (string(char)+string(matrix[x+1][y])+string(matrix[x+2][y])+string(matrix[x+3][y]) == "XMAS" || string(char)+string(matrix[x+1][y])+string(matrix[x+2][y])+string(matrix[x+3][y]) == "SAMX") {
+			if (x >= 1 && x <= len(matrix)-2) && (y >= 1 && y <= len(line)-2) && (string(matrix[x-1][y-1])+string(char)+string(matrix[x+1][y+1]) == "SAM") && (string(matrix[x+1][y-1])+string(char)+string(matrix[x-1][y+1]) == "SAM") {
 				xmasFound++
 			}
-			// Diagonal right
-			if (y <= len(line)-4 && x <= len(matrix)-4) && (string(char)+string(matrix[x+1][y+1])+string(matrix[x+2][y+2])+string(matrix[x+3][y+3]) == "XMAS" || string(char)+string(matrix[x+1][y+1])+string(matrix[x+2][y+2])+string(matrix[x+3][y+3]) == "SAMX") {
+			if (x >= 1 && x <= len(matrix)-2) && (y >= 1 && y <= len(line)-2) && (string(matrix[x-1][y-1])+string(char)+string(matrix[x+1][y+1]) == "SAM") && (string(matrix[x-1][y+1])+string(char)+string(matrix[x+1][y-1]) == "SAM") {
 				xmasFound++
 			}
-			// Diagonal left
-			if (y >= 3 && x <= len(matrix)-4) && (string(char)+string(matrix[x+1][y-1])+string(matrix[x+2][y-2])+string(matrix[x+3][y-3]) == "XMAS" || (string(char)+string(matrix[x+1][y-1])+string(matrix[x+2][y-2])+string(matrix[x+3][y-3]) == "SAMX")) {
+			if (x >= 1 && x <= len(matrix)-2) && (y >= 1 && y <= len(line)-2) && (string(matrix[x-1][y-1])+string(char)+string(matrix[x+1][y+1]) == "MAS") && (string(matrix[x-1][y+1])+string(char)+string(matrix[x+1][y-1]) == "MAS") {
 				xmasFound++
 			}
 		}
